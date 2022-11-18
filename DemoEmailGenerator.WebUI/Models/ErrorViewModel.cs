802038e41956035c7510b3dc4720153a0c92ed74
yoloowen <cpro.ovo@gmail.com>
2022-11-18 23:50:22 +0100

    initial commit

---
+++ DemoEmailGenerator.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoEmailGenerator.WebUI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
